92ee7a4a143328ea0646443abc53b4576f652922
cindy6810 <167484719+cindy6810@users.noreply.github.com>
2024-04-21 09:55:35 -0600

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,79 +1,69 @@
+class Genre:
+    GENRE_NAMES = {
+        0: "Romance",
+        1: "Mystery",
+        2: "Science Fiction",
+        3: "Thriller",
+        4: "Young Adult",
+        5: "Children's Fiction",
+        6: "Self-help",
+        7: "Fantasy",
+        8: "Historical Fiction",
+        9: "Poetry"
+    }
+   
+    def __init__(self, genre_code):
+        self.genre_code = genre_code
 
-import os.path
-import csv
-
-
-from book import Book
-from genre import genre_table
-
-additional_menu = False
-
-def find_book_by_isbn(books, isbn) -> Book:
-    for book in books:
-        if isbn in book.isbn:
-            return book
-
-
-def print_books(found):
-    for book in found:
-        print(book)
-        
-def add_book(books):
-    isbn = input("Enter ISBN: ")
-    title = input("Enter Title: ")
-    author = input("Enter Author: ")
-    genre_name = input("Enter Genre name: ")
-    for gen_key in genre_table:
-        if genre_table.get(gen_key) == genre_name:
-            book = Book(isbn, title, author, gen_key, True)
-            books.append(book)
-            print("new book added.")
-            return
-    print("invalid Genre name.")
-
-
-def find_value_in_dict(dictionary, value):
-    for key in dictionary:
-        if dictionary.get(key) == value:
-            return key
-    return None
-
-
-def remove_book(books):
-    isbn = input("Enter ISBN: ")
-    book = find_book_by_isbn(books, isbn)
-    if book is not None:
-        books.remove(book)
-        print(f"remove book {book.title} successfully!")
-    else:
-        print(f"Invalid ISBN {isbn}.")
-    return
-
-
-def save_books(books, filename) -> int:
-    with open('books.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['isbn', 'title', 'author', 'genre', 'available']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        # writer.writeheader()
-        for book in books:
-            writer.writerow({'isbn': book.isbn,
-                             'title': book.title,
-                             'author': book.author,
-                             'genre': book.genre,
-                             'available': book.available})
-    return len(books)
-
-
-if __name__ == "__main__":
-    __books = []
-    __filename = ""
-    while True:
-        __filename = input("Please type file path and press enter: ")
-        if os.path.isfile(__filename):
-            break
-        else:
-            print(f"not found file '{__filename}', please check file path!")
-
-    books_count = load_books(__books, __filename)
-    print(f"file '{__filename}' processed, {books_count} record(s) loaded.")
+class Book:
+    def __init__(self, isbn, title, author, genre, available):
+        self.__isbn = isbn
+        self.__title = title
+        self.__author = author
+        self.__genre = int(genre)
+        self.__available = bool(available)
+    
+    # Define getters
+    def get_isbn(self):
+        return self.__isbn
+   
+    def get_title(self):
+        return self.__title
+   
+    def get_author(self):
+        return self.__author
+   
+    def get_genre(self):
+        return self.__genre
+   
+    def get_available(self):
+        return self.__available
+   
+    def get_genre_name(self):
+        return Genre.GENRE_NAMES.get(self.__genre, "N/A")
+   
+    def get_availability(self):
+        return "Available" if self.__available == True else "Borrowed"
+    
+    # Define setters
+    def set_isbn(self, new_isbn):
+        self.__isbn = new_isbn
+   
+    def set_title(self, new_title):
+        self.__title = new_title
+   
+    def set_author(self, new_author):
+        self.__author = new_author
+   
+    def set_genre(self, new_genre):
+        self.__genre = new_genre
+   
+    def borrow_it(self):
+        self.__available = False
+   
+    def return_it(self):
+        self.__available = True
+   
+    # Define how the information wil be displayed
+    def __str__(self):
+        return f"{self.__isbn} {self.__title:<25} {self.__author:<25} {self.get_genre_name():<20} {self.get_availability()}"
